a8541587596b932a360a072338e9d60eda74c4da
Akari Takahashi <akaritakahashioss@gmail.com>
2022-07-10 19:26:33 +0900

    v0.0.7

--- privatemessengerclient/src/main/java/mn/akari/maven/privatemessengerclient/Client.java
+++ privatemessengerclient/src/main/java/mn/akari/maven/privatemessengerclient/Client.java
@@ -1,11 +1,11 @@
 // @title Private Messenger Client (GUI)
-// @version 0.0.6
+// @version 0.0.7
 // @author Takahashi Akari <akaritakahashioss@gmail.com>
 // @date 2022-07-09
 // @description This is a private messenger client. using Swing.
 // @license MIT License
 // @copyright (c) 2022 Takahashi Akari <akaritakahashioss@gmail.com>
-// @url <https://takahashi-akari.github.io/privateMessenger/>
+// @url <https://takahashi-akari.github.io/PrivateMessenger/>
 // @see https://github.com/takahashi-akari/PrivateMessenger-Server/blob/main/privatemessengerserver/src/main/java/mn/akari/maven/privatemessengerserver/App.java
 // @see https://github.com/takahashi-akari/PrivateMessenger-Server/blob/main/privatemessengerserver/src/main/java/mn/akari/maven/privatemessengerserver/Constsans.java
 // @see https://github.com/takahashi-akari/PrivateMessenger-Server/blob/main/README.md
